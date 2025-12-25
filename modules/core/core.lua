96e4725aa5dd57a09f3d11ad1c1bd2859c2a504e
JUMAEDI SAPUTRA <acokhook@gmail.com>
2025-12-26 03:38:45 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- modules/core/core.lua
+++ modules/core/core.lua
@@ -1611,7 +1611,7 @@ FishingAreas = {
 function GetEventGUI()
     local success, gui = pcall(function()
         local menuRings = workspace:WaitForChild("!!! MENU RINGS", 5)
-        local eventTracker = menuRings:WaitForChild("Event Tracker", 5)
+        local eventTracker = workspace:WaitForChild("Event Tracker", 5)
         local contentItems = eventTracker.Main.Gui.Content.Items
 
         local countdown = contentItems.Countdown:WaitForChild("Label")	
@@ -1635,4 +1635,5 @@ function GetEventGUI()
         return nil
     end
 end
+
 
